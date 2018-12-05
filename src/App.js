b43f68423add155db9d72316e697e7e0158de7f3
Ciminera <l.ciminera@mylight-systems.com>
2018-12-05 17:00:46 +0100

    use graph-app-kit and neo4j-driver to retrieve db schema

--- src/App.js
+++ src/App.js
@@ -1,20 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import "semantic-ui-css/semantic.min.css";
+
+import {
+    GraphAppBase,
+    ConnectModal,
+    CONNECTED
+} from "graph-app-kit/components/GraphAppBase";
+
+import { Cypher } from "graph-app-kit/components/Cypher";
+
+import neo4j from "neo4j-driver";
+
+const integrationPoint = window.neo4jDesktopApi;
+
+const MyApp = ({ connectionState }) =>
+    connectionState === CONNECTED ? (
+        <Cypher
+            query="call db.schema"
+            render={({ pending, error, result: schema }) => {
+                return pending
+                    ? "pending"
+                    : error
+                        ? error.message
+                        : JSON.stringify(schema);
+            }}
+        />
+    ) : (
+        <h1>Please connect</h1>
+    );
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Popoto.js Neo4j Graph App
-          </p>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <GraphAppBase
+                driverFactory={neo4j}
+                integrationPoint={integrationPoint}
+                render={({ connectionState, connectionDetails, setCredentials }) => {
+                    return [
+                        <ConnectModal
+                            key="modal"
+                            errorMsg={connectionDetails ? connectionDetails.message : ""}
+                            onSubmit={setCredentials}
+                            show={connectionState !== CONNECTED}
+                        />,
+                        <MyApp key="app" connectionState={connectionState} />
+                    ];
+                }}
+            />
+        );
+    }
 }
 
 export default App;
